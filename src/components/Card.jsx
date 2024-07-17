a0ed9c9dde386c4ca7f21717029478aa3f2d26af
Alxvarp <ingvargasalexander@gmail.com>
2024-07-16 21:56:17 -0500

    Create a Card Component

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import { useState} from 'react'
+import  './Card.css'
+
+function Card(){
+
+    return (
+        <>
+          <div className="card">
+            <p>Monda</p>
+          </div>
+        </>
+      )
+
+}
+
+export default Card
